5021f2c4786a54787e3ff3c051ab0945d42730de
Howard Wang <idontwannarock@gmail.com>
2022-04-15 15:20:49 +0800

    feat: added init.lua and  lua/user/options.lua

---
+++ lua/user/options.lua
@@ -0,0 +1,11 @@
+-- :help options
+vim.opt.clipboard = "unnamedplus" -- allows neovim to access the system clipboard
+vim.opt.cmdheight = 2             -- more space in the neovim command line for displaying messages
+vim.opt.conceallevel = 0          -- so that `` is visible in markdown file
+vim.opt.fileencoding = "utf-8"    -- the encoding written to a file
+vim.opt.swapfile = false          -- creates a swapfile
+vim.opt.updatetime = 300          -- faster completion (4000ms default)
+vim.opt.cursorline = true         -- highlight the cursor line
+vim.opt.number = true             -- set numbered lines
+vim.opt.relativenumber = true     -- set relative numbered lines
+vim.opt.tabstop = 4               -- insert spaces for a tab (8 default)
