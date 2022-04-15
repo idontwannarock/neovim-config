555443d147a05afae6dd8e790b026ebc383fd6fa
Howard Wang <idontwannarock@gmail.com>
2022-04-15 15:38:04 +0800

    refactor: changed config settings in options.lua to table type config

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,11 +1,19 @@
 -- :help options
-vim.opt.clipboard = "unnamedplus" -- allows neovim to access the system clipboard
-vim.opt.cmdheight = 2             -- more space in the neovim command line for displaying messages
-vim.opt.conceallevel = 0          -- so that `` is visible in markdown file
-vim.opt.fileencoding = "utf-8"    -- the encoding written to a file
-vim.opt.swapfile = false          -- creates a swapfile
-vim.opt.updatetime = 300          -- faster completion (4000ms default)
-vim.opt.cursorline = true         -- highlight the cursor line
-vim.opt.number = true             -- set numbered lines
-vim.opt.relativenumber = true     -- set relative numbered lines
-vim.opt.tabstop = 4               -- insert spaces for a tab (8 default)
+local options = {
+    clipboard = "unnamedplus", -- allows neovim to access the system clipboard
+    cmdheight = 2,             -- more space in the neovim command line for displaying messages
+    conceallevel = 0,          -- so that `` is visible in markdown file
+    fileencoding = "utf-8",    -- the encoding written to a file
+    swapfile = false,          -- creates a swapfile
+    updatetime = 300,          -- faster completion (4000ms default)
+    cursorline = true,         -- highlight the cursor line
+    number = true,             -- set numbered lines
+    relativenumber = true,     -- set relative numbered lines
+    tabstop = 4,               -- insert spaces for a tab (8 default)
+	shiftwidth = 4,            -- the number of spaces inserted for each indentation
+}
+
+for k, v in pairs(options) do
+    vim.opt[k] = v
+end
+
