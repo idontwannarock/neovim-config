fd0cd8fc8aca8e00cfe06667d4ee5c67b1c49624
Howard Wang <idontwannarock@gmail.com>
2022-05-18 15:04:32 +0800

    fix: edit treesitter.lua to amend deprecated config

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -3,10 +3,12 @@ if not status_ok then
 	return
 end
 
+require "nvim-treesitter.install".compilers = { "clang" }
+
 configs.setup {
-	ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+	ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
 	sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-	ignore_install = { "" }, -- list of parsers to ignore installing
+	ignore_install = { "phpdoc" }, -- list of parsers to ignore installing
 	highlight = {
 		enable = true, -- false will disable the whole extention
 		disable = { "" }, -- list of languages that will be disabled
